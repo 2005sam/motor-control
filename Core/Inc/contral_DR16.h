c5838b450528c863d9307296d561e3f3a1d75d9c
sam2005 <shengpengxiang1@outlook.com>
2025-07-16 11:17:09 +0800

    build:DR16控制器

--- Core/Inc/contral_DR16.h
+++ Core/Inc/contral_DR16.h
@@ -2,8 +2,36 @@
 #define CONTROL_DR16_H
 #include "stm32f4xx_hal.h"
 #include "CMSIS_os.h"
+#include "FreeRTOS.h"
 
-void ControlDR16Init(void);
-void ControlDR16Process(void);
+//this struct is used to hold the processed data from DR16
+//ch0-3 reflect the control channel 1-4,range from 364 to 1684
+//s1, s2 reflect the control transmitter s1, s2,1-up,2-down,3-middle
+//mouse_x_axis, mouse_y_axis, mouse_z_axis reflect the mouse speed in x, y, z axis
+//range from -32768 to 32767
+//mouse_button_L, mouse_button_R, reflect the mouse button state
+//0 is not pressed, 1 is pressed
+//button reflect to W, A, S, D, Q, E, R, F, G, H in the keyboard
+struct ControlDR16Data
+{
+  uint16_t ch0;
+  uint16_t ch1;
+  uint16_t ch2;
+  uint16_t ch3;
+  char s1;
+  char s2;
+  int16_t mouse_x_axis;
+  int16_t mouse_y_axis;
+  int16_t mouse_z_axis;
+  char mouse_button_L;
+  char mouse_button_R;
+  uint16_t buttons;
+  uint16_t reserved;
+};
+
+void ControlDR16Init(UART_HandleTypeDef *huart);
+void ControlDR16GetValue(struct ControlDR16Data *data);
+
+
 
 #endif
--- Core/Src/contral_DR16.c
+++ Core/Src/contral_DR16.c
@@ -3,66 +3,77 @@
 uint8_t rxBuffer[18];
 UART_HandleTypeDef huart;
 QueueHandle_t control_dr16_queue_isr;
+struct ControlDR16Data processed_data;
 struct PreControlDR16Data
 {
-    uint64_t data0;
-    uint64_t data1;
-    uint16_t data2;
-};
-struct ControlDR16Data
-{
-    uint16_t ch0;
-    uint16_t ch1;
-    uint16_t ch2;
-    uint16_t ch3;
-    char s1;
-    char s2;
-    int16_t mouse_x_axis;
-    int16_t mouse_y_axis;
-    int16_t mouse_z_axis;
-    char mouse_button_L;
-    char mouse_button_R;
-    char mouse_button_M;
-    uint16_t buttons;
-    uin
+  uint64_t data0;
+  uint64_t data1;
+  uint16_t data2;
 };
 
-void ControlDR16Init(void)
+void ControlDR16Process(void *argument);
+void ControlDR16Init(UART_Typedef *param_huart)
 {
-    // Initialize the UART peripheral
-    huart.Instance = USART3;
-    huart.Init.BaudRate = 115200;
-    huart.Init.WordLength = UART_WORDLENGTH_8B;
-    huart.Init.StopBits = UART_STOPBITS_1;
-    huart.Init.Parity = UART_PARITY_NONE;
-    huart.Init.Mode = UART_MODE_TX_RX;
-    huart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-    huart.Init.OverSampling = UART_OVERSAMPLING_16;
-    HAL_UART_Init(&huart);
-    HAL_UART_Receive_IT(&huart, (uint8_t *)rxBuffer, sizeof(rxBuffer));
-
-    // Create a queue to hold the received data
-    control_dr16_queue_isr = xQueueCreate(2, sizeof(struct PreControlDR16Data));
+  huart = *param_huart;
+  // Initialize the UART for receiving data from DR16
+  HAL_UART_Receive_IT(&huart, (uint8_t *)rxBuffer, sizeof(rxBuffer));
+  // Create a queue to hold the received data
+  control_dr16_queue_isr = xQueueCreate(2, sizeof(struct PreControlDR16Data));
+  xTaskCreate(ControlDR16Process, "ControlDR16Process", 256, NULL, 1, NULL);
 }
 
 void ControlDR16Process(void *argument)
 {
-    struct PreControlDR16Data control_dr16_data;
-    while (1)
-    {
-        xQueueReceive(control_dr16_queue_isr, &control_dr16_data, portMAX_DELAY);
-        // Process the received data
-    }
+  struct PreControlDR16Data control_dr16_data;
+  while (1)
+  {
+    xQueueReceive(control_dr16_queue_isr, &control_dr16_data, portMAX_DELAY);
+    // Process the received data
 
-    void HAL_UART_RxCpltCallback(UART_HandleTypeDef * huart)
-    {
-        if (huart->Instance == USART3)
-        {
-            struct PreControlDR16Data control_dr16_data;
-            control_dr16_data.data0 = rxBuffer[0] || rxBuffer[1] << 8 || rxBuffer[2] << 16 || rxBuffer[3] << 24;
-            control_dr16_data.data1 = rxBuffer[4] || rxBuffer[5] << 8 || rxBuffer[6] << 16 || rxBuffer[7] << 24;
-            control_dr16_data.data2 = rxBuffer[16] || rxBuffer[17] << 8;
-            xQueueSendFromISR(control_dr16_queue_isr, control_dr16_data, NULL);
-            HAL_UART_Receive_IT(&huart, (uint8_t *)rxBuffer, sizeof(rxBuffer));
-        }
-    }
+    processed_data.ch0 = control_dr16_data.data0 & 0x007F;
+    processed_data.ch1 = (control_dr16_data.data0 >> 11) & 0x007F;
+    processed_data.ch2 = (control_dr16_data.data0 >> 22) & 0x007F;
+    processed_data.ch3 = (control_dr16_data.data0 >> 33) & 0x007F;
+    processed_data.s1 = (control_dr16_data.data0 >> 44) & 0x01;
+    processed_data.s2 = (control_dr16_data.data0 >> 46) & 0x01;
+    processed_data.mouse_x_axis = (control_dr16_data.data0 >> 48) & 0xFFFF;
+    processed_data.mouse_y_axis = (control_dr16_data.data1 & 0xFFFF);
+    processed_data.mouse_z_axis = (control_dr16_data.data1 >> 16) & 0xFFFF;
+    processed_data.mouse_button_L = (control_dr16_data.data1 >> 32) & 0x00FF;
+    processed_data.mouse_button_R = (control_dr16_data.data1 >> 40) & 0x00FF;
+    processed_data.buttons = (control_dr16_data.data1 >> 48) & 0xFFFF;
+    processed_data.reserved = control_dr16_data.data2 & 0xFFFF;
+  }
+}
+
+void ControlDR16GetValue(struct ControlDR16Data *data)
+{
+  *data = processed_data;
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+  if (huart->Instance == USART3)
+  {
+    struct PreControlDR16Data control_dr16_data;
+    control_dr16_data.data0 = rx_Buffer[0] |
+                              rx_Buffer[1] << 8 |
+                              rx_Buffer[2] << 16 |
+                              rx_Buffer[3] << 24 |
+                              rx_Buffer[4] << 32 |
+                              rx_Buffer[5] << 40 |
+                              rx_Buffer[6] << 48 |
+                              rx_Buffer[7] << 56;
+    control_dr16_data.data1 = rx_Buffer[8] |
+                              rx_Buffer[9] << 8 |
+                              rx_Buffer[10] << 16 |
+                              rx_Buffer[11] << 24 |
+                              rx_Buffer[12] << 32 |
+                              rx_Buffer[13] << 40 |
+                              rx_Buffer[14] << 48 |
+                              rx_Buffer[15] << 56;
+    control_dr16_data.data2 = rx_Buffer[16] | (rx_Buffer[17] << 8);
+    xQueueSendFromISR(control_dr16_queue_isr, control_dr16_data, NULL);
+    HAL_UART_Receive_IT(&huart, (uint8_t *)rxBuffer, sizeof(rxBuffer));
+  }
+}
